2efe819f79a98b3804f21d83fc913ab4c52298ae
skywhywayy <artjom.po360@gmail.com>
2026-06-27 16:36:25 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function orderProduct(productName, price) {
-    const message = "Hi! I want to order: " + productName + ". Price: " + price;
-    const url = "https://t.me/skylinewayy?text=" + encodeURIComponent(message);
-    window.open(url, '_blank');
-}
